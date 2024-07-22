e6438486e05fa700e50918125e7012899ab81f54
Sumeyye Aydogan <sumeyyednm1@gmail.com>
2024-07-22 10:05:58 +0300

    key spaces removed

--- serveraes.cpp
+++ serveraes.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 #include "fileaes.h"
 
-#define keyaes "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c" // key for encryption and decryption
+#define keyaes "2b7e151628aed2a6abf7158809cf4f3c" // key for encryption and decryption
 
 #pragma comment(lib, "Ws2_32.lib")
 
