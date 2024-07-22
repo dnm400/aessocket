d1a781aa7446ff7b830955e9d410a8193b7f8b9b
Sumeyye Aydogan <sumeyyednm1@gmail.com>
2024-07-22 10:40:06 +0300

    include

--- fileaes.h
+++ fileaes.h
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <cstdint>
+#include <algorithm>
+#include <cstring>
+#include <cctype> // for isspace
 
 using namespace std;
 string crypt(string textin, string keyin, uint8_t CTR[16]);
